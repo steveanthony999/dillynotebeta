2c4bc7837744815ff1a11fb234b5092b0e6052ea
Steven Woodward <steveanthony999@gmail.com>
2021-02-08 18:36:36 -0800

    created function to post categories to database

--- functions/index.js
+++ functions/index.js
@@ -28,3 +28,30 @@ exports.getCategories = functions.https.onRequest((req, res) => {
 });
 
 // Create category
+exports.createCategory = functions.https.onRequest((req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(400).json({ error: 'Method not allowed' });
+  }
+
+  const newCategory = {
+    title: req.body.title,
+    username: req.body.username,
+    userId: req.body.userId,
+    parentId: null,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    list: [],
+    path: [],
+  };
+
+  admin
+    .firestore()
+    .collection('categories')
+    .add(newCategory)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'something went wrong' });
+      console.error(err);
+    });
+});
